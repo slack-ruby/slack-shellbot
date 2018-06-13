92e80e07b8b642a8fa24b20c95f931dba80fda91
dblock <dblock@dblock.org>
2018-06-13 07:38:15 -0400

    Attempt to restart dead teams.

--- slack-shellbot/app.rb
+++ slack-shellbot/app.rb
@@ -1,4 +1,32 @@
 module SlackShellbot
   class App < SlackRubyBotServer::App
+    include Celluloid
+
+    def after_start!
+      every 60 * 60 do
+        ping_teams!
+      end
+    end
+
+    private
+
+    def ping_teams!
+      Team.active.each do |team|
+        begin
+          ping = team.ping!
+          next if ping[:presence].online
+          logger.warn "DOWN: #{team}"
+          after 60 do
+            ping = team.ping!
+            unless ping[:presence].online
+              logger.info "RESTART: #{team}"
+              SlackShellbot::Service.instance.start!(team)
+            end
+          end
+        rescue StandardError => e
+          logger.warn "Error pinging team #{team}, #{e.message}."
+        end
+      end
+    end
   end
 end
